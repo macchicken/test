4d9afc3fb210f7f5d98705da28081872cf86af90
Barry <renault194@hotmail.com>
2014-09-02 12:12:52 +0800

    finished the Array2 problems

--- src/codingbat/Array2.java
+++ src/codingbat/Array2.java
@@ -287,6 +287,89 @@ public class Array2 {
 		return count==total;
 	}
 
+	private boolean more14(int[] nums){
+		int oneC=0,fourC=0;
+		for (int num:nums){
+			if (num==1){oneC++;}
+			else if (num==4){fourC++;}
+		}
+		return oneC>fourC;
+	}
+
+	private int[] fizzArray(int n){
+		int[] result=new int[n];
+		for (int i = 0; i < result.length; i++) {
+			result[i]=i;
+		}
+		return result;
+	}
+
+	private boolean only14(int[] nums){
+		for (int num:nums){
+			if (num!=1&&num!=4){return false;}
+		}
+		return true;
+	}
+
+	private String[] fizzArray2(int n){
+		String[] result=new String[n];
+		for (int i = 0; i < result.length; i++) {
+			result[i]=String.valueOf(i);
+		}
+		return result;
+	}
+
+	private boolean no14(int[] nums){
+		int oneC=0,fourC=0;
+		for (int num:nums) {
+			if (num==1){oneC++;}
+			else if (num==4){fourC++;}
+		}
+		return oneC==0||fourC==0;
+	}
+
+	private boolean isEverywhere(int[] nums, int val){
+		if (nums.length==2){return val==nums[0]||val==nums[1];}
+		int i=0;
+		boolean hasPrev=false;
+		while(i<nums.length){
+			if (nums[i]!=val&&hasPrev){return false;}
+			else if (nums[i]==val){hasPrev=true;i+=2;}
+			else{i++;}
+		}
+		return true;
+	}
+
+	private boolean either24(int[] nums){
+		if (nums.length<2){return false;}
+		boolean has22=false,has44=false;
+		int len=nums.length;
+		for (int i = 0; i < len; i++) {
+			if (nums[i]==2&&i+1<len&&nums[i+1]==2){has22=true;}
+			else if (nums[i]==4&&i+1<len&&nums[i+1]==4){has44=true;}
+		}
+		return has22||has44?!(has22&&has44):false;
+	}
+
+	private int matchUp(int[] nums1, int[] nums2){
+		if (nums1.length!=nums2.length){return 0;}
+		int diff=0,count=0;
+		for (int i = 0; i < nums1.length; i++) {
+			diff=Math.abs(nums1[i]-nums2[i]);
+			if (diff==2||diff==1){count++;}
+		}
+		return count;
+	}
+
+	private boolean has12(int[] nums){
+		boolean hasOne=false;
+		for (int num:nums){
+			if (hasOne&&num==2){return true;}
+			if (num==1){hasOne=true;}
+		}
+		return false;
+	}
+
 	private int bigDiff(int[] nums){
 		if (nums!=null){
 			int len=nums.length;
@@ -586,6 +669,108 @@ public class Array2 {
 		System.out.println(false==sum28(nums3));
 		int[] nums4={};
 		System.out.println(false==sum28(nums4));
+	}
+
+	public void testmore14(){
+		int[] nums={1, 4, 1};
+		System.out.println(true==more14(nums));
+		int[] nums2={1, 4, 1, 4};
+		System.out.println(false==more14(nums2));
+		int[] nums3={1, 1};
+		System.out.println(true==more14(nums3));
+	}
+
+	public void testfizzArray(){
+		int[] expected={0,1,2,3};
+		System.out.println(Arrays.equals(expected, fizzArray(4)));
+		int[] expected2={0};
+		System.out.println(Arrays.equals(expected2, fizzArray(1)));
+		int[] expected3={0,1,2,3,4,5,6,7,8,9};
+		System.out.println(Arrays.equals(expected3, fizzArray(10)));
+	}
+
+	public void testonly14(){
+		int[] nums={1, 4, 1, 4};
+		System.out.println(true==only14(nums));
+		int[] nums2={1, 4, 2, 4};
+		System.out.println(false==only14(nums2));
+		int[] nums3={1, 1};
+		System.out.println(true==only14(nums3));
+	}
+
+	public void testfizzArray2(){
+		String[] expected={"0", "1", "2", "3"};
+		System.out.println(Arrays.equals(expected, fizzArray2(4)));
+		String[] expected2={"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+		System.out.println(Arrays.equals(expected2, fizzArray2(10)));
+		String[] expected3= {"0", "1"};
+		System.out.println(Arrays.equals(expected3, fizzArray2(2)));
+	}
+
+	public void testno14(){
+		int[] nums={1, 2, 3};
+		System.out.println(true==no14(nums));
+		int[] nums2={1, 2, 3, 4};
+		System.out.println(false==no14(nums2));
+		int[] nums3={2, 3, 4};
+		System.out.println(true==no14(nums3));
+	}
+
+	public void testisEverywhere(){
+		/**
+		 * We'll say that a value is "everywhere" in an array
+		 * if for every pair of adjacent elements in the array,
+		 * at least one of the pair is that value.
+		 * Return true if the given value is everywhere in the array. 
+		 */
+		int[] nums={1, 2, 1, 3};
+		System.out.println(true==isEverywhere(nums,1));
+		int[] nums2={1, 2, 1, 3};
+		System.out.println(false==isEverywhere(nums2,2));
+		int[] nums3={1, 2, 1, 3, 4};
+		System.out.println(false==isEverywhere(nums3,1));
+		int[] nums4={3, 1};
+		System.out.println(true==isEverywhere(nums4,3));
+		int[] nums5={3, 1};
+		System.out.println(false==isEverywhere(nums5,2));
+		int[] nums6={3};
+		System.out.println(true==isEverywhere(nums6,1));
+		int[] nums7={};
+		System.out.println(true==isEverywhere(nums7,1));
+	}
+
+	public void testeither24(){
+		int[] nums={1, 2, 2};
+		System.out.println(true==either24(nums));
+		int[] nums2={4, 4, 1};
+		System.out.println(true==either24(nums2));
+		int[] nums3={4, 4, 1, 2, 2};
+		System.out.println(false==either24(nums3));
+	}
+
+	public void testmatchUp(){
+		int[] nums11={1, 2, 3};
+		int[] nums12={2, 3, 10};
+		System.out.println(2==matchUp(nums11,nums12));
+		int[] nums21={1, 2, 3};
+		int[] nums22={2, 3, 5};
+		System.out.println(3==matchUp(nums21,nums22));
+		int[] nums31={1, 2, 3};
+		int[] nums32={2, 3, 3};
+		System.out.println(2==matchUp(nums31,nums32));
+	}
+
+	public void testhas12(){
+		int[] nums={1, 3, 2};
+		System.out.println(true==has12(nums));
+		int[] nums2={3, 1, 2};
+		System.out.println(true==has12(nums2));
+		int[] nums3={3, 1, 4, 5, 2};
+		System.out.println(true==has12(nums3));
+		int[] nums4={1, 1, 4, 5, 1};
+		System.out.println(false==has12(nums4));
+		int[] nums5={2, 1, 4, 5, 1};
+		System.out.println(false==has12(nums5));
 	}
 
 	public void testbigDiff(){
--- src/codingbat/BatQ.java
+++ src/codingbat/BatQ.java
@@ -7,7 +7,7 @@ public class BatQ {
 	public static void main(String[] args) {
 		String packageName="codingbat.";
 		try {
-			PlayTest.runTest(packageName+"Array2", "sum28");
+			PlayTest.runTest(packageName+"Array2", "has12");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
