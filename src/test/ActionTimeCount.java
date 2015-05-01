5086649cb113be3f352918eca47f15255d921969
Barry <renault194@hotmail.com>
2015-05-02 00:04:17 +1000

    use try-with statement in 1.7

--- src/test/ActionTimeCount.java
+++ src/test/ActionTimeCount.java
@@ -8,26 +8,27 @@ public class ActionTimeCount {
 
 	private static final long MAXTIME=10*1000;
 	private static long timeCount=0;
-	private static String str="";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		String str = null;BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(timeCount<MAXTIME){
-			long sysDate = System.currentTimeMillis();
-			try{
-				while("".equals((str = br.readLine()))){
-					
-				};
-				}catch(IOException e){
-				System.out.println("Input error:" + e.getMessage());
-				}finally{
-					timeCount+=(System.currentTimeMillis()-sysDate);
-					if (timeCount>=MAXTIME){str="";break;}
-				}
-				System.out.println("input: '" + str + "'");
-				str="";
-				System.out.println("timeCount: '" + timeCount + "'");
+		String str = null;
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+			while(timeCount<MAXTIME){
+				long sysDate = System.currentTimeMillis();
+				try{
+					while("".equals((str = br.readLine()))){
+						
+					};
+					}catch(IOException e){
+					System.out.println("Input error:" + e.getMessage());
+					}finally{
+						timeCount+=(System.currentTimeMillis()-sysDate);
+						if (timeCount>=MAXTIME){str="";break;}
+					}
+			}
+			System.out.println("input: '" + str + "'");
+			str="";
+			System.out.println("timeCount: '" + timeCount + "'");
 		}
 	}
 
